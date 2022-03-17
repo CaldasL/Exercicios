8df6e04b1c3ea48fdfbad0179faaf0c0d4c01855
CaldasL <101758265+CaldasL@users.noreply.github.com>
2022-03-16 22:41:26 -0300

    Add files via upload

---
+++ Ex 4.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+namespace Exercicio_4
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando Variaveis
+            string nomeAluno;
+            double notaProva1;
+            double notaProva2;
+            double notaProva3;
+            double pesoNotaProva1 = 2;
+            double pesoNotaProva2 = 4;
+            double pesoNotaProva3 = 6;
+            double mediaponderada;
+
+            //Ler Teclado
+            Console.Write("Informe o nome do aluno:");
+            nomeAluno = Console.ReadLine();
+            Console.Write("Informe a nota da prova 1:");
+            notaProva1 = double.Parse(Console.ReadLine());
+            Console.Write("Informe a nota da prova 2:");
+            notaProva2 = double.Parse(Console.ReadLine());
+            Console.Write("Informe a nota da prova 3:");
+            notaProva3 = double.Parse(Console.ReadLine());
+
+            //Exibir media ponderada
+           
+            mediaponderada = (notaProva1 * pesoNotaProva1 + notaProva2 * pesoNotaProva2 + notaProva3 * pesoNotaProva3) / (pesoNotaProva1 + pesoNotaProva2 + pesoNotaProva3);
+
+            Console.WriteLine($"Aluno: {nomeAluno} " +
+                $"\nMedia Ponderada: {mediaponderada}"); 
+
+           
+             
+
+
+
+
+        }
+    }
+}
---
+++ Ex3.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+namespace Exercicio_3
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarando variaveis
+            string nome;
+            double salario;
+            double vendas;
+            double salarioComComissao;
+
+            //ler teclado
+            Console.WriteLine("Informe o nome do vendedor");
+            nome = Console.ReadLine();
+            Console.WriteLine("Informe o salario fixo do vendedor");
+            salario = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Informe o total em vendas do(a) {nome} no mês");
+            vendas = double.Parse(Console.ReadLine());
+
+            //exibir os dados
+            salarioComComissao = salario + (0.2 * vendas);
+
+            Console.WriteLine($"Vendedor: {nome}" +
+                $"\nSalario: {salario}" +
+                $"\nSalario Final: {salarioComComissao}");
+
+        }
+    }
+}
